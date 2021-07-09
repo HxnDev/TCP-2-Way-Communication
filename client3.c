09c3d67acf20e0808e3dc536b5181da923241bcf
Hassan Shahzad <83294673+HxnDev@users.noreply.github.com>
2021-07-09 09:54:49 +0500

    Add files via upload

---
+++ client3.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+int main()
+{
+	
+	char request[256] = "Hello I am Client are you there";
+	char buf[200];
+	
+	// create the socket
+	int sock;
+	sock = socket(AF_INET, SOCK_STREAM, 0);
+	
+	//setup an address
+	struct sockaddr_in server_address;
+	server_address.sin_family = AF_INET;
+	server_address.sin_addr.s_addr = INADDR_ANY;
+	server_address.sin_port = htons(3001);
+
+	connect(sock, (struct sockaddr *) &server_address, sizeof(server_address));
+	
+	
+	char st1[30]="exit";
+	do
+	{
+		printf("enter message ");
+	
+		scanf("%s",&request);
+
+		send(sock, request, sizeof(request), 0);
+		recv(sock, &buf, sizeof(buf), 0);
+		printf("%s",&buf);
+	}
+	while(strcmp(request,st1));
+	
+
+	
+	close(sock);
+
+	return 0;
+}
